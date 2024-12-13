227decc92c28eb53864cfcd32b22f9ea3fa1ff75
Gabriella-Samra <gabriella.samra@hotmail.co.uk>
2024-12-13 19:04:04 +0000

    Created a new array to store the instances of boats

--- BattleshipGame.Core/Game.cs
+++ BattleshipGame.Core/Game.cs
@@ -11,13 +11,17 @@ namespace BattleshipGame.Core
         {
             Console.WriteLine("Game Starting");
             var gameGrid = new GameGrid();
+            var boatList = new List<Boat>{};
             Console.WriteLine("Initial Grid:");
             Console.WriteLine($"The Grid Height is {gameGrid.YAxis}");
             Console.WriteLine($"The Grid Height is {gameGrid.XAxis}");
 
             var largeBoat = new Boat("Large");
+            boatList.Add(largeBoat);
             var mediumBoat = new Boat("Medium");
+            boatList.Add(mediumBoat);
             var smallBoat = new Boat("Small");
+            boatList.Add(smallBoat);
 
             Console.WriteLine($"The size of my large boat is {largeBoat.BoatLength()}");
             Console.WriteLine($"The size of my medium boat is {mediumBoat.BoatLength()}");
