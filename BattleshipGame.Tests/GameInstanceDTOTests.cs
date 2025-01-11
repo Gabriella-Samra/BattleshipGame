427dc6a01d7c8da2f0462636b15b220be2e860e4
Gabriella-Samra <gabriella.samra@hotmail.co.uk>
2025-01-11 00:27:52 +0000

    added test file for gameinstanceDTO

---
+++ BattleshipGame.Tests/GameInstanceDTOTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using BattleshipGame.Core;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BattleshipGame.Tests
+{
+    [TestFixture]
+    public class GameInstanceDTOTests
+    {
+    
+    }
+}
