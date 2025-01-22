3e0d2c0e4b5aa033a62f18e476778d6aafbb15a1
Gabriella-Samra <gabriella.samra@hotmail.co.uk>
2025-01-22 16:03:39 +0000

    updated

--- BattleshipGame.Tests/obj/Debug/net8.0/BattleshipGame.Tests.AssemblyInfo.cs
+++ BattleshipGame.Tests/obj/Debug/net8.0/BattleshipGame.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleshipGame.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a699671601333b6b0750688f258c8d870e5af18b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e55250ff8ff0b22c3bd7dfeb7b8a678e651740e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleshipGame.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleshipGame.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
