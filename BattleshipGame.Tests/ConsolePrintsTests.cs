d8e4fbefc1dcd5e7c8ed78764ae9bdcaf3820df4
Gabriella-Samra <gabriella.samra@hotmail.co.uk>
2025-01-29 15:25:02 +0000

    Added test to check hits are displaying properly on the grid

--- BattleshipGame.Tests/ConsolePrintsTests.cs
+++ BattleshipGame.Tests/ConsolePrintsTests.cs
@@ -89,7 +89,67 @@ namespace BattleshipGame.Tests
         }
 
         [Test]
-        public void CheckUpdatedGameGridPrintWorksWhenAMissIsGuesses()
+        public void CheckUpdatedGameGridPrintWorksWhenAHitIsGuessed()
+        {
+            var gameGrid = new GameGrid();
+            var game = new Game();
+
+            var smallBoat = new Boat("Small");
+            var mediumBoat = new Boat("Medium");
+            var largeBoat = new Boat("Large");
+
+            var boatList = new List<Boat>
+            {
+                smallBoat,
+                mediumBoat,
+                largeBoat
+            };
+
+            // Assign coordinates to boats
+            boatList[0].BoatCoordinates.Add(new Coordinate(0, 0));
+            boatList[1].BoatCoordinates.Add(new Coordinate(1, 0));
+            boatList[1].BoatCoordinates.Add(new Coordinate(1, 1));
+            boatList[2].BoatCoordinates.Add(new Coordinate(2, 0));
+            boatList[2].BoatCoordinates.Add(new Coordinate(2, 1));
+            boatList[2].BoatCoordinates.Add(new Coordinate(2, 2));
+
+            foreach (var boat in boatList)
+            {
+                boat.BoatCoordinates = boat.BoatCoordinates.Distinct().ToList();
+            }
+
+            // Update the game grid
+            GameGrid.UpdateGameGridWithBoats(gameGrid, boatList);
+
+            // Generate and test output
+
+            Coordinate hitCoord = new Coordinate(0,0);
+            gameGrid = GameGrid.UpdateGameGridWithHits(gameGrid, hitCoord);
+            
+            var updatedGrid = ConsolePrints.UpdatesWithBoatGridPrint(gameGrid);
+
+            var expected = string.Join(Environment.NewLine, new[]
+            {
+                " X  || M,B || L,B || 3,0 || 4,0 || 5,0 || 6,0 || 7,0 || 8,0 || 9,0",
+                "0,1 || M,B || L,B || 3,1 || 4,1 || 5,1 || 6,1 || 7,1 || 8,1 || 9,1",
+                "0,2 || 1,2 || L,B || 3,2 || 4,2 || 5,2 || 6,2 || 7,2 || 8,2 || 9,2",
+                "0,3 || 1,3 || 2,3 || 3,3 || 4,3 || 5,3 || 6,3 || 7,3 || 8,3 || 9,3",
+                "0,4 || 1,4 || 2,4 || 3,4 || 4,4 || 5,4 || 6,4 || 7,4 || 8,4 || 9,4",
+                "0,5 || 1,5 || 2,5 || 3,5 || 4,5 || 5,5 || 6,5 || 7,5 || 8,5 || 9,5",
+                "0,6 || 1,6 || 2,6 || 3,6 || 4,6 || 5,6 || 6,6 || 7,6 || 8,6 || 9,6",
+                "0,7 || 1,7 || 2,7 || 3,7 || 4,7 || 5,7 || 6,7 || 7,7 || 8,7 || 9,7",
+                "0,8 || 1,8 || 2,8 || 3,8 || 4,8 || 5,8 || 6,8 || 7,8 || 8,8 || 9,8",
+                "0,9 || 1,9 || 2,9 || 3,9 || 4,9 || 5,9 || 6,9 || 7,9 || 8,9 || 9,9",
+            });
+
+            var normalizedUpdatedGrid = updatedGrid.Replace("\r\n", "\n").Trim();
+            var normalizedExpected = expected.Replace("\r\n", "\n").Trim();
+
+            Assert.That(normalizedUpdatedGrid, Is.EqualTo(normalizedExpected));
+        }
+
+        [Test]
+        public void CheckUpdatedGameGridPrintWorksWhenAMissIsGuessed()
         {
             var gameGrid = new GameGrid();
             var game = new Game();
--- BattleshipGame.Tests/obj/Debug/net8.0/BattleshipGame.Tests.AssemblyInfo.cs
+++ BattleshipGame.Tests/obj/Debug/net8.0/BattleshipGame.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleshipGame.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96803e3d5ff7279bfd37a584bfdab12c871c721e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc6714ee54c205d738d16f35a093a4257633c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleshipGame.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleshipGame.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
